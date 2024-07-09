b60b9200de70a835c66fe4a62fa2e67e0109d53b
vale2034 <botte.2034421@studenti.uniroma1.it>
2024-07-09 04:35:35 +0200

    rimozioni e nuovi modelli

--- penalty_shootout/main.js
+++ penalty_shootout/main.js
@@ -3,7 +3,7 @@ import { GLTFLoader } from './utils/GLTFLoader.js';  // Assicurati che il percor
 import { OrbitControls } from './utils/OrbitControls.js';
 
 
-var scene, camera, renderer, light, mesh, controls;
+var scene, camera, renderer, light, mesh, controls, player;
 let loader;
 
 var moveForward = false, moveBackward = false, moveLeft = false, moveRight = false;
@@ -12,13 +12,12 @@ var cameraCoordinates = document.getElementById('camera-coordinates');
 
 // Inizializza la scena, la camera e il renderer
 scene = new THREE.Scene();
-// Aggiungere un cielo azzurro
+//scene.background = new THREE.Color(0xFFFFFF);  // Aggiungere un cielo azzurro
 
 
-// Carica l'immagine JPEG per la skybox
-loader = new THREE.TextureLoader();
-const texture = loader.load('assets/sunflowers_puresky.jpg');
-
+//Carica l'immagine JPEG per la skybox
+let load = new THREE.TextureLoader();
+const texture = load.load('assets/sunflowers_puresky.jpg');
 scene.background = texture;
 
 renderer = new THREE.WebGLRenderer();
@@ -32,7 +31,7 @@ scene.add(light);
 
 
 //camera e sua posizione 
-camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 10000000000000000);
 camera.position.set(-8.53,5.90,-17.12);  // Posizionare la camera all'altezza dell'omino
 
 
@@ -42,91 +41,239 @@ controls.target.set(-8, 1, 10);  // Punto di interesse iniziale della camera
 controls.update();  // Necessario se la camera è stata modificata manualmente
 
 
+let namesToRemove = [
+    'LayerLayer_1', 'LayerLayer_1_1', 'Cylinder001', 'Cylinder002', 'Cylinder003',
+    'Box001', 'Box002', 'Box003', 'Box004', 'Box005', 'Box006', 'Box007',
+    'Box008', 'Box009', 'Box010', 'Box011',   "mixamorigHips_00", 'Alpha_Surface', 'Box013',
+    "mixamorigSpine_01",
+    "mixamorigSpine1_02",
+    "mixamorigSpine2_03",
+    "mixamorigNeck_04",
+    "mixamorigHead_05",
+    "mixamorigHeadTop_End_06",
+    "mixamorigLeftEye_07",
+    "mixamorigRightEye_08",
+    "mixamorigLeftShoulder_09",
+    "mixamorigLeftArm_010",
+    "mixamorigLeftForeArm_011",
+    "mixamorigLeftHand_012",
+    "mixamorigLeftHandMiddle1_013",
+    "mixamorigLeftHandMiddle2_014",
+    "mixamorigLeftHandMiddle3_015",
+    "mixamorigLeftHandMiddle4_016",
+    "mixamorigLeftHandThumb1_017",
+    "mixamorigLeftHandThumb2_018",
+    "mixamorigLeftHandThumb3_019",
+    "mixamorigLeftHandThumb4_020",
+    "mixamorigLeftHandIndex1_021",
+    "mixamorigLeftHandIndex2_022",
+    "mixamorigLeftHandIndex3_023",
+    "mixamorigLeftHandIndex4_024",
+    "mixamorigLeftHandRing1_025",
+    "mixamorigLeftHandRing2_026",
+    "mixamorigLeftHandRing3_027",
+    "mixamorigLeftHandRing4_028",
+    "mixamorigLeftHandPinky1_029",
+    "mixamorigLeftHandPinky2_030",
+    "mixamorigLeftHandPinky3_031",
+    "mixamorigLeftHandPinky4_032",
+    "mixamorigRightShoulder_033",
+    "mixamorigRightArm_034",
+    "mixamorigRightForeArm_035",
+    "mixamorigRightHand_036",
+    "mixamorigRightHandMiddle1_037",
+    "mixamorigRightHandMiddle2_038",
+    "mixamorigRightHandMiddle3_039",
+    "mixamorigRightHandMiddle4_040",
+    "mixamorigRightHandThumb1_041",
+    "mixamorigRightHandThumb2_042",
+    "mixamorigRightHandThumb3_043",
+    "mixamorigRightHandThumb4_044",
+    "mixamorigRightHandIndex1_045",
+    "mixamorigRightHandIndex2_046",
+    "mixamorigRightHandIndex3_047",
+    "mixamorigRightHandIndex4_048",
+    "mixamorigRightHandRing1_049",
+    "mixamorigRightHandRing2_050",
+    "mixamorigRightHandRing3_051",
+    "mixamorigRightHandRing4_052",
+    "mixamorigRightHandPinky1_053",
+    "mixamorigRightHandPinky2_054",
+    "mixamorigRightHandPinky3_055",
+    "mixamorigRightHandPinky4_056",
+    "mixamorigRightUpLeg_057",
+    "mixamorigRightLeg_058",
+    "mixamorigRightFoot_059",
+    "mixamorigRightToeBase_060",
+    "mixamorigRightToe_End_061",
+    "mixamorigLeftUpLeg_062",
+    "mixamorigLeftLeg_063",
+    "mixamorigLeftFoot_064",
+    "mixamorigLeftToeBase_065",
+    "mixamorigLeftToe_End_066",
+    'Object_7',
+    'Object_8',
+    'Object_9'
+];
+
+
 //carica modello
 loader = new GLTFLoader();
-loader.load(
-    'models/penalty_area/scene.gltf',
-    function (gltf) {
-        gltf.scene.traverse(function (child) {
-            
-            if (child.name === 'Layer:Layer 1') {
-                // Rimuovi il nodo dalla scena
-                child.parent.remove(child);
+// Funzione per caricare e scalare il modello
+function loadModelAndScale(url, scale, namesToRemove) {
+    loader.load(
+        url,
+        function (gltf) {
+
+            removeObjects(gltf, namesToRemove);
+
+            // Applica la trasformazione di scala
+            gltf.scene.scale.set(scale, scale, scale);
+
+            // Aggiungi il modello alla scena
+            scene.add(gltf.scene);
+
+
+            // Modifica la posizione di Mario o Luigi se necessario
+            if (url.includes('luigi')) {
+                console.log('LuigiPosition' , gltf.scene.position)
+                gltf.scene.position.set(-43, 0, 70); // Imposta la posizione di Luigi a (5, 0, 0)
+                console.log('LuigiRotation' , gltf.scene.rotation)
+                gltf.scene.rotation.set(0,3.1,0);
+            } else if (url.includes('mario')) {
+                console.log('MarioPosition' , gltf.scene.position)
+                gltf.scene.position.set(-43, 0, -55); // Imposta la posizione di Mario a (-5, 0, 0)
+            } else if (url.includes('penalty_area')) {
+                const prato = gltf.scene.getObjectByName('Plane001')
+                const palla = gltf.scene.getObjectByName('Sphere001')
+                scene.add(prato);
+                //palla.scale.set(0,0,0)
+                prato.position.set(25, 0, 0);
+                console.log("Scale", prato.scale);
+                prato.scale.set(1, 0.6, 1);
+                exploreScene(scene);
             }
-            
-    
-        });
+        },
+        function (xhr) {
+            console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+        },
+        function (error) {
+            console.error('Error loading model', error);
+            console.error(error.stack);
+        }
+    );
+}
 
-        // Aggiungi la scena GLTF alla scena principale
-        scene.add(gltf.scene);
-
-    },
-    function (xhr) {
-        console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-    },
-    function (error) {
-        console.error('Error loading model', error);
-        console.error(error.stack);
-    }
-);
+// Esempio di utilizzo per caricare e scalare i modelli
+loadModelAndScale('models/penalty_area/scene.gltf', 5, namesToRemove); // Scala 0.1 (10% della dimensione originale)
+loadModelAndScale('models/luigi/scene.gltf', 1.2, []); // Scala 0.05 (5% della dimensione originale)
+loadModelAndScale('models/mario/scene.gltf', 0.2, []); // Scala 0.05 (5% della dimensione originale)
 
 
-// Funzione per creare una nuvola
-function createCloud(x, y, z) {
-    const cloudGeometry = new THREE.PlaneGeometry(5, 5);
-    const cloudMaterial = new THREE.MeshBasicMaterial({
-        map: new THREE.TextureLoader().load('path/to/cloud/texture.png'),
-        transparent: true
+
+
+function printNodeNames(object, level = 0) {
+    let indent = ' '.repeat(level * 2);
+    console.log(indent + object.name);
+    object.children.forEach(child => {
+        printNodeNames(child, level + 1);
     });
-    const cloud = new THREE.Mesh(cloudGeometry, cloudMaterial);
-    cloud.position.set(x, y, z);
-    scene.add(cloud);
 }
 
-// Crea alcune nuvole
-createCloud(0, 5, -10);
-createCloud(5, 7, -15);
-createCloud(-5, 6, -12);
 
 
-/*Ascolta il click per calciare la palla
-window.addEventListener('click', kickBall);
-// Funzione per calciare la palla
-function kickBall() {
-    const targetPosition = { x: 0, y: 0.2, z: 10 };
-    const tween = new TWEEN.Tween(ball.position).to(targetPosition, 1000);
-    tween.start();
-}*/
+//Funzione per esplorare la scena
+function exploreScene(scene) {
+    scene.traverse(function (child) {
+        console.log('Object:', child.name);
+        console.log('Position:', child.position);
+        //console.log('Rotation:', child.rotation);
+        //console.log('Scale:', child.scale);
+        //console.log('Visibility:', child.visible);
+        //console.log('Material:', child.material); // Assicurati che il materiale sia accessibile correttamente
 
-// Ascolta il click per calciare la palla
-//window.addEventListener('click', kickBall);
-
-// Anima la scena
-function animate() {
-    requestAnimationFrame(animate);
-
-    if (moveForward) velocity.z -= 0.1;
-    if (moveBackward) velocity.z += 0.1;
-    if (moveLeft) velocity.x -= 0.1;
-    if (moveRight) velocity.x += 0.1;
-
-    camera.position.add(velocity);
-    velocity.multiplyScalar(0.9);  // Damping
-
-    TWEEN.update();
-    controls.update();  // Necessario per aggiornare la posizione della camera
-
-
-    cameraCoordinates.innerHTML = `Camera Position: X=${camera.position.x.toFixed(2)}, Y=${camera.position.y.toFixed(2)}, Z=${camera.position.z.toFixed(2)}`;
-
-    renderer.render(scene, camera);
+        // Esplora il genitore (parent)
+        if (child.parent) {
+            console.log('Parent:', child.parent.name);
+        } else {
+            console.log('No parent');
+        }
+    });
 }
 
-animate();
+function traverseHierarchy(object, playerName) {
+    let player = null;
 
+    object.traverse(function(child) {
+        // Trova l'oggetto principale dell'omino
+        if (child.name === playerName) {
+            player = child;
+        }
 
-document.addEventListener('keydown', onKeyDown);
-document.addEventListener('keyup', onKeyUp);
+        // Esempio: raccogli anche i genitori e i figli diretti dell'omino
+        if (player && (child.parent === player || child.parent === player.parent)) {
+            console.log('Parent:', child.parent ? child.parent.name : null);
+            console.log('Object:', child.name);
+            console.log('Position:', child.position);
+            console.log('Rotation:', child.rotation);
+            console.log('Scale:', child.scale);
+            console.log('Visibility:', child.visible);
+            console.log('Material:', child.material); // Se l'oggetto ha un materiale applicato
+            console.log('---');
+        }
+    });
+
+    return player;
+}
+
+// Funzione per rimuovere gli oggetti indesiderati dalla scena
+function removeObjects(gltf, namesToRemove) {
+    let objectsToRemove = [];
+
+    gltf.scene.traverse(function (child) {
+        if (namesToRemove.includes(child.name)) {
+            objectsToRemove.push(child);
+        }
+    });
+
+    objectsToRemove.forEach(function (child) {
+        if (child.parent) {
+            //console.log('Removing object:', child.name);
+            child.parent.remove(child);
+        } else {
+            console.warn('Parent is undefined for object:', child.name);
+        }
+    });
+
+    /*gltf.scene.traverse(function (child) {
+        console.log('After removal check - object:', child.name);
+    });*/
+
+}
+
+function addObjects(gltf, namesToAdd) {
+    let objectsToAdd = [];
+
+    gltf.scene.traverse(function (child) {
+        if (namesToAdd.includes(child.name)) {
+            objectsToAdd.push(child);
+        }
+    });
+
+    objectsToAdd.forEach(function (child) {
+        if (child.parent) {
+            console.log('Adding object:', child.name);
+            child.parent.add(child);
+        } else {
+            console.warn('Parent is undefined for object:', child.name);
+        }
+    });
+
+    gltf.scene.traverse(function (child) {
+        console.log('After adding check - object:', child.name);
+    });
+
+}
 
 function onKeyDown(event) {
     switch (event.code) {
@@ -170,3 +317,30 @@ function onKeyUp(event) {
     }
 }
 
+document.addEventListener('keydown', onKeyDown);
+document.addEventListener('keyup', onKeyUp);
+
+
+
+// Anima la scena
+function animate() {
+    requestAnimationFrame(animate);
+
+    if (moveForward) velocity.z -= 0.1;
+    if (moveBackward) velocity.z += 0.1;
+    if (moveLeft) velocity.x -= 0.1;
+    if (moveRight) velocity.x += 0.1;
+
+    camera.position.add(velocity);
+    velocity.multiplyScalar(0.9);  // Damping
+
+    TWEEN.update();
+    controls.update();  // Necessario per aggiornare la posizione della camera
+
+
+    cameraCoordinates.innerHTML = `Camera Position: X=${camera.position.x.toFixed(2)}, Y=${camera.position.y.toFixed(2)}, Z=${camera.position.z.toFixed(2)}`;
+
+    renderer.render(scene, camera);
+}
+
+animate();
